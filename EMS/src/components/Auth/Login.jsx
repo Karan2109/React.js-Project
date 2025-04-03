96b23f8ef88a67418ff9dedfc09c159c8892ea5a
Karan Singh <karans21.dev@gmail.com>
2025-04-03 16:21:18 +0530

    done with designing Login form

---
+++ EMS/src/components/Auth/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form submitted!");
+    console.log(`Email is ${email}`);
+    console.log(`Password is ${password}`);
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <div className="border-2 border-emerald-600 rounded-2xl">
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+            className="flex flex-col items-center justify-center p-20"
+          >
+            <input
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              required
+              className="border-2 border-emerald-600 rounded-full py-3 px-5 text-xl outline-none placeholder:text-gray-400"
+              type="email"
+              placeholder="Enter your email"
+            />
+            <input
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className="border-2 border-emerald-600 rounded-full py-3 px-5 text-xl outline-none placeholder:text-gray-400 mt-3"
+              type="password"
+              placeholder="Enter password"
+            />
+            <button className="border-none bg-emerald-600 rounded-full py-3 px-5 text-xl outline-none placeholder:text-white mt-7 w-full hover:bg-emerald-500">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
