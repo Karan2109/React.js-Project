c3019a66f68abaa81896412365f29647d3fd2422
Karan Singh <karans21.dev@gmail.com>
2025-04-04 10:30:20 +0530

    Done with Alltask

---
+++ EMS/src/components/other/AllTask.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+const AllTask = () => {
+  return (
+    <div id="task" className="bg-[#1c1c1c] p-5 rounded mt-5 h-90 overflow-auto">
+      <div className="bg-red-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-pink-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-orange-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-green-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-red-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-pink-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-orange-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-green-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded ">
+        <h2>Karan</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  );
+};
+
+export default AllTask;
