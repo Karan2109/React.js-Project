a22f5f1c8e61b259a0905ee7947d7cd91586ff9a
Karan Singh <karans21.dev@gmail.com>
2025-04-05 14:02:12 +0530

    now fetching data using props

--- EMS/src/components/other/Header.jsx
+++ EMS/src/components/other/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Header = () => {
+const Header = ({ data }) => {
   return (
     <div>
       <h1 className="flex items-end justify-between">
         <h1 className="text-2xl font-medium">
-          Hello, <br /> <span className="text-3xl font-semibold">Karan 👋</span>
+          Hello, <br />{" "}
+          <span className="text-3xl font-semibold">{data.firstname} 👋</span>
         </h1>
         <button className="bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-lg">
           Logout
