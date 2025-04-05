d2d9c5147965baa0545232ebfecf682807ae8e28
Karan Singh <karans21.dev@gmail.com>
2025-04-05 19:40:14 +0530

    Designed of Alltask is completed

--- EMS/src/components/other/AllTask.jsx
+++ EMS/src/components/other/AllTask.jsx
@@ -1,77 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
 
 const AllTask = () => {
+  const authData = useContext(AuthContext);
   return (
-    <div id="task" className="bg-[#1c1c1c] p-5 rounded mt-5 h-90 overflow-auto">
+    <div id="task" className="bg-[rgb(28,28,28)] p-5 rounded mt-5 h-90">
       <div className="bg-red-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
+        <h2 className="text-xl w-1/6 text-center font-bold">Employee Name</h2>
+        <h3 className="text-xl w-1/6 text-center font-bold">New Task</h3>
+        <h5 className="text-xl w-1/6 text-center font-bold">Active Task</h5>
+        <h5 className="text-xl w-1/6 text-center font-bold">Completed</h5>
+        <h5 className="text-xl w-1/6 text-center font-bold">Failed</h5>
       </div>
-      <div className="bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-pink-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-orange-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-green-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-red-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-pink-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-orange-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-green-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded ">
-        <h2>Karan</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
+      <div id="task1" className="h-[80%] overflow-auto">
+        {authData.employees.map(function (elem) {
+          return (
+            <div className="bg-emerald-300 mb-2 py-2 px-4 flex justify-between rounded ">
+              <h2 className="w-1/6 font-semibold bg-orange-500 text-center text-xl">
+                {elem.firstname}
+              </h2>
+              <h3 className="w-1/6 font-semibold bg-blue-500 text-center text-xl">
+                {elem.taskStats.newTask}
+              </h3>
+              <h5 className="w-1/6 font-semibold bg-yellow-500 text-center text-xl">
+                {elem.taskStats.active}
+              </h5>
+              <h5 className="w-1/6 font-semibold bg-green-500 text-center text-xl">
+                {elem.taskStats.completed}
+              </h5>
+              <h5 className="w-1/6 font-semibold bg-red-500 text-center text-xl">
+                {elem.taskStats.failed}
+              </h5>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
