3bcbf8150b278349c2529a614560a32c782e225e
Karan Singh <karans21.dev@gmail.com>
2025-04-11 10:48:47 +0530

    Added Social media links to the Navbar

--- KSPortfolio/src/components/Navbar.jsx
+++ KSPortfolio/src/components/Navbar.jsx
@@ -11,11 +11,45 @@ const Navbar = () => {
       <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 w-12" src={logo} alt="logo" width={80} />
       </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl text-white">
+      {/* <div className="m-8 flex items-center justify-center gap-4 text-2xl text-white">
         <FaLinkedin />
         <FaGithub />
         <FaTwitter />
         <FaInstagram />
+      </div> */}
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl text-white">
+        <a
+          href="https://www.linkedin.com/in/singhkarann"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-blue-600 transition-colors duration-300"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/Karan2109"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-400 transition-colors duration-300"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://twitter.com/your-username"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-sky-500 transition-colors duration-300"
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href="https://instagram.com/_karan.s.s"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-pink-500 transition-colors duration-300"
+        >
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
