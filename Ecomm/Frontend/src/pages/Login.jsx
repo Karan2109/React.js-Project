c366961e85ea5dc9a571e00be42eebb30a33a9e7
Karan Singh <karans21.dev@gmail.com>
2025-04-24 16:42:27 +0530

    just checking the user authencation

--- Ecomm/Frontend/src/pages/Login.jsx
+++ Ecomm/Frontend/src/pages/Login.jsx
@@ -6,6 +6,11 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("User Login: ", { email, password });
+  };
+
   return (
     <div className="flex">
       {/* <div className="w-full md:w-1/2 flex-col justify-center items-center p-8 md:p-12">
@@ -52,7 +57,10 @@ const Login = () => {
         </form>
       </div> */}
       <div className="w-full md:w-1/2 flex flex-col items-center justify-center p-8 md:p-12">
-        <form className="w-full max-w-md bg-white p-8 rounded-lg border border-gray-300 shadow-sm">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md bg-white p-8 rounded-lg border border-gray-300 shadow-sm"
+        >
           <div className="flex justify-center mb-6">
             <h2 className="text-xl font-medium">M-Vastra</h2>
           </div>
