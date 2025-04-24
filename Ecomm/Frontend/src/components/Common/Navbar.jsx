fead0d360700bb9a3d652f48c9ef6d07bc6e801e
Karan Singh <karans21.dev@gmail.com>
2025-04-24 12:12:09 +0530

    just a basic change for rounting

--- Ecomm/Frontend/src/components/Common/Navbar.jsx
+++ Ecomm/Frontend/src/components/Common/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
         </div>
         {/* Right- Search and Cart */}
         <div className="flex items-center space-x-4">
-          <Link to="/profile" className="hover:text-black">
+          <Link to="/login" className="hover:text-black">
             <HiOutlineUser className="h-6 w-6 text-gray-700" />
           </Link>
           <button
