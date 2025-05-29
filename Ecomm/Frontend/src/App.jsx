4cd3c5460ffa4f121f7bc1bb6a8572ba528b1817
Karan Singh <karans21.dev@gmail.com>
2025-05-29 17:14:08 +0530

    app.jsx

--- Ecomm/Frontend/src/App.jsx
+++ Ecomm/Frontend/src/App.jsx
@@ -21,6 +21,7 @@ import OrderManagement from "./components/Admin/OrderManagement";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import ProtectedfRoute from "./components/Common/ProtectedfRoute";
 
 const App = () => {
   return (
@@ -47,7 +48,14 @@ const App = () => {
             <Route path="order/:id" element={<OrderDetailsPage />} />
             <Route path="/my-orders" element={<MyOrdersPage />} />
           </Route>
-          <Route path="/admin" element={<AdminLayout />}>
+          <Route
+            path="/admin"
+            element={
+              <ProtectedfRoute role="admin">
+                <AdminLayout />
+              </ProtectedfRoute>
+            }
+          >
             {/* Admin Layout */}
             <Route index element={<AdminHomePage />} />
             <Route path="users" element={<UserManagement />} />
