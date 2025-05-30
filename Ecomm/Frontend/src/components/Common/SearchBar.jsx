f6d2502618b64848be7ba37a5a2ef842e104869b
Karan Singh <karans21.dev@gmail.com>
2025-05-30 15:07:31 +0530

    SearchBar.jsx

--- Ecomm/Frontend/src/components/Common/SearchBar.jsx
+++ Ecomm/Frontend/src/components/Common/SearchBar.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import { useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { HiMiniXMark } from "react-icons/hi2";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  fetchProductsByFilter,
+  setFilters,
+} from "../../redux/slices/productsSlice";
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSearchToggle = () => {
     setIsOpen(!isOpen);
@@ -13,7 +21,11 @@ const SearchBar = () => {
 
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log("Search term:", searchTerm);
+    // console.log("Search term:", searchTerm);
+    dispatch(setFilters({ search: searchTerm }));
+    dispatch(fetchProductsByFilter({ search: searchTerm }));
+    navigate(`/collections/all?search=${searchTerm}`);
+
     setIsOpen(false); // Close the search bar after submitting
   };
 
